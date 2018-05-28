c51a41e5308864fe4d7e2776160e98564e2bbf9a
yinshijie <631518264@qq.com>
2018-05-28 11:21:48 +0800

    JS加载超时时间

--- assets/js/require-config.js
+++ assets/js/require-config.js
@@ -20,8 +20,6 @@ var require_config ={
         validform: '/assets/js/jquery/Validform_v5.3.2',
         clipboard: '/assets/js/clipboard/ZeroClipboard',
         tagsinput: '/assets/js/bootstrap/bootstrap-tagsinput.min',
-        ckeditor_base: '/assets/js/ckeditor/ckeditor',
-        ckeditor: '/assets/js/ckeditor/adapters/jquery',
         moment: '/assets/js/moment.min',
         dateRangePicker: '/assets/js/bootstrap/daterangepicker',
         calendar: '/assets/js/calendar/lhgcalendar.min',
@@ -74,10 +72,6 @@ var require_config ={
         },
         tagsinput: {
             deps: ['jquery']
-        },
-        ckeditor: {
-            deps: ['jquery', 'ckeditor_base'],
-            exports: 'CKEditor'
         },
         calendar: {
             deps: ['jquery'],
